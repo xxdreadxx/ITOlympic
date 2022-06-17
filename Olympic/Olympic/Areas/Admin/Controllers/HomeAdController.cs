e056a1d8f58a8572929e952fce60bcb589108781
mennt <mennt@huongvietgroup.com>
2022-06-17 17:41:56 +0700

    commit temp

---
+++ Olympic/Olympic/Areas/Admin/Controllers/HomeAdController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Olympic.Areas.Admin.Controllers
+{
+    public class HomeAdController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
