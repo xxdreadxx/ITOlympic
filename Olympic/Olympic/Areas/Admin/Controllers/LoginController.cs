da8ca98f6b584e3001e66829b6112cd7a9bbdac5
Hieu Cao Trung <theend.spirit@gmail.com>
2022-06-22 23:11:26 +0700

    commit

--- Olympic/Olympic/Areas/Admin/Controllers/LoginController.cs
+++ Olympic/Olympic/Areas/Admin/Controllers/LoginController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Models;
+using Models.EF;
+using Models.DAO;
 
 namespace Olympic.Areas.Admin.Controllers
 {
     public class LoginController : Controller
     {
+        UserDao userDao = new UserDao();
         // GET: Admin/Login
         public ActionResult Index()
         {
@@ -17,6 +21,33 @@ namespace Olympic.Areas.Admin.Controllers
         public ActionResult Resetpass()
         {
             return View();
+        }
+
+        public JsonResult Login(string username, string pass)
+        {
+            bool kt = true;
+            string mess = "Đăng nhập thành công!";
+            int kq = userDao.Login(username, pass);
+            if (kq == 0)
+            {
+                kt = false;
+                mess = "Tài khoản hoặc mật khẩu không chính xác, đăng nhập thất bại!";
+            }
+            else if (kq == 2)
+            {
+                kt = false;
+                mess = "Tài khoản đang bị khóa, vui lòng liên hệ quản trị viên để mở khóa!";
+            }
+            else if (kq == 3)
+            {
+                kt = false;
+                mess = "Tài khoản hết hạn sử dụng, vui lòng liên hệ quản trị viên để gia hạn!";
+            }
+            return Json(new
+            {
+                status = kt,
+                mess = mess
+            }, JsonRequestBehavior.AllowGet);
         }
     }
 }
