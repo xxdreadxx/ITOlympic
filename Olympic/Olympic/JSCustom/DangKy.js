bdb5bfc1017c31554b36849690a9737234f5391c
mennt <mennt@huongvietgroup.com>
2022-07-14 17:44:05 +0700

    commit

---
+++ Olympic/Olympic/JSCustom/DangKy.js
@@ -0,0 +1,11 @@
+﻿$('document').ready(function () {
+    $('#navDangKy').addClass('active');
+    var firstchild = $('#accordion li:first').attr('data-id');
+    ShowTimeline(firstchild);
+});
+function ShowTimeline(id) {
+    $('.list-group-item').removeClass('active');
+    $('#a_' + id).addClass('active');
+    $('.clskithi').css('display', 'none');
+    $('.clsid_' + id).css('display', 'flex');
+}
---
+++ Olympic/Olympic/Models/CuocThiHangMuc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Olympic.Models
+{
+    public class CuocThiHangMuc
+    {
+        public int ID { get; set; }
+        public int IDCuocThi { get; set; }
+        public string TenCuocThi { get; set; }
+        public string TenHangMuc { get; set; }
+        public string ThoiGianBatDau { get; set; }
+        public string ThoiGianKetThuc { get; set; }
+        public string ThoiGianBatDauNhanHS { get; set; }
+        public string ThoiGianKetThucNhanHS { get; set; }
+        public byte TrangThai { get; set; }
+    }
+}
