8c102df8a98eb644ea1ad0ca508209f4ddaa6ae8
mennt <mennt@huongvietgroup.com>
2022-08-12 09:43:59 +0700

    commit

---
+++ Olympic/Olympic/Models/ThongKe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Olympic.Models
+{
+    public class ThongKe
+    {
+        public int SoLuong { get; set; }
+        public int Nam { get; set; }
+    }
+}
